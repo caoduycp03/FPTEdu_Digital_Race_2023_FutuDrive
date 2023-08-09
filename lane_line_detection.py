69010fdb6e6331422e99646d5f6adff438487569
Cao Duy <caoduycp03@gmail.com>
2023-08-09 16:16:01 +0700

    dieu kien dung re

--- lane_line_detection.py
+++ lane_line_detection.py
@@ -30,10 +30,9 @@ def birdview_transform(img):
     warped_img = cv2.warpPerspective(img, M, (WIDTH, HEIGHT)) # Image warping
     return warped_img
 
-
-def find_left_right_points(image, draw=None):
+def find_left_right_points(roi, image, draw=None):
     # Consider the position 70% from the top of the image
-    interested_line_y = int(HEIGHT * 0.7)
+    interested_line_y = int(HEIGHT * roi)
     if draw is not None:
         cv2.line(draw, (0, interested_line_y),
                  (WIDTH, interested_line_y), (0, 0, 255), 2)
@@ -72,48 +71,6 @@ def find_left_right_points(image, draw=None):
                 draw, (int(right_point), interested_line_y), 3, (0, 255, 0), -1)
 
     return left_point, right_point
-def find_left_right_points_2(image, draw=None):
-    # Consider the position 70% from the top of the image
-    interested_line_y = int(HEIGHT * 0.58)
-    if draw is not None:
-        cv2.line(draw, (0, interested_line_y),
-                 (WIDTH, interested_line_y), (0, 0, 255), 2)
-    interested_line = image[interested_line_y, :]
-
-    # Detect left/right points
-    left_point = -1
-    right_point = -1
-    lane_width = 48
-    center = WIDTH // 2
-
-    for x in range(0, WIDTH, 1):
-        if interested_line[x] > 0.6:
-            left_point = x
-            break
-    for x in range(WIDTH-1, 0, -1):
-        if interested_line[x] > 0.6:
-            right_point = x
-            break
-    '''this end point method apply for 128x128 images, if the height of new image is different
-        multiply the end point with new_height/last_height'''
-    
-    end_point = (68 - (HEIGHT - interested_line_y))/(68 - 0.2*HEIGHT) *17
-    if left_point < end_point and right_point != -1:
-        left_point = left_point - ((WIDTH/2) - (right_point - left_point) )
-
-    if right_point > (128 - end_point) and left_point != -1:
-        right_point = right_point + ((WIDTH/2) - (right_point - left_point) )
-
-    if draw is not None:
-        if left_point != -1:
-            draw = cv2.circle(
-                draw, (int(left_point), interested_line_y), 3, (255, 255, 0), -1)
-        if right_point != -1:
-            draw = cv2.circle(
-                draw, (int(right_point), interested_line_y), 3, (0, 255, 0), -1)
-
-    return left_point, right_point
-
 
 pred = 0
 def detect_lane_model(model, img):
@@ -131,12 +88,12 @@ def detect_lane_model(model, img):
 time_to_turn = False
 sign_list = []
 
-def check_turn():
-    global time_to_turn, sign_list, angle_turn
-    time.sleep(2)
-    time_to_turn = False
-    sign_list = []
-    angle_turn = 90
+# def check_turn():
+#     global time_to_turn, sign_list, angle_turn
+#     time.sleep(5)
+#     time_to_turn = False
+#     sign_list = []
+#     angle_turn = 90
 
 sign = "hello"
 angle_turn = 90
@@ -152,16 +109,17 @@ def calculate_control_signal(img, signs, draw=None):
     cv2.imshow("pred_birdview", pred_birdview)
     cv2.waitKey(1)
     draw[:, :] = birdview_transform(draw)
-    left_point, right_point = find_left_right_points(pred_birdview, draw=draw)
-    left_point_2, right_point_2 = find_left_right_points_2(pred_birdview, draw=draw)
+    left_point, right_point = find_left_right_points(0.6, pred_birdview, draw=draw)
+    left_point_2, right_point_2 = find_left_right_points(0.7, pred_birdview, draw=draw)
+    left_point_3, right_point_3 = find_left_right_points(0.2, pred_birdview, draw=draw)
 
     angle_degrees = 90
-
+    car_location = WIDTH//2
+    center_road = (left_point_3 + right_point_3)/2
 ############### control when detect sign
     if signs:
-        print("----------------------------" ,signs)
-        # a = 150
         sign = signs[-1][0]
+        print(sign)
         sign_list.append(sign)
 
         if st.mode(sign_list) == 'left':
@@ -171,15 +129,19 @@ def calculate_control_signal(img, signs, draw=None):
     
     if len(sign_list) > 0:    
         if (st.mode(sign_list) == 'left' and left_point_2 - left_point > 5) or (st.mode(sign_list) == 'right' and right_point_2 - right_point > 5):
-            print('angle', angle_turn)
             time_to_turn = True
-            thread = threading.Thread(target= check_turn)
-            thread.start()
+            # thread = threading.Thread(target= check_turn)
+            # thread.start()
     
-    # if (right_point_2 - left_point_2) - (right_point - left_point) < 5:
-    #     time_to_turn = False
-
+    if time_to_turn and abs(center_road - car_location) < 20:
+    #abs(np.sum(pred_birdview[:,:WIDTH//2-1]) - np.sum(pred_birdview[:,WIDTH//2:WIDTH-1])) < 20: 
+        time_to_turn = False
+        sign_list = []
+        angle_turn = 90
+        print('duy dep trai')
+    
     if time_to_turn:
+        print(angle_turn)
         return angle_turn
 ###############
 
